d7492bb630cf984c5f07e43cad37076c94f452c6
Henco Appel <hencoappel@gmail.com>
2015-08-06 18:40:05 +0100

    Just work on being able to set the pixel data to the canvas

--- src/mandelbrot/core.cljs
+++ src/mandelbrot/core.cljs
@@ -9,6 +9,38 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn jsprint [args]
+  (.log js/console args))
+
+(defn genvals [width height]
+  (mapcat conj
+            (repeat (vector 255 0 0))
+            (take (* width height) (cycle (range 255)))))
+
+
+(defn setImageData [image data]
+   (let [idv (map vector (iterate inc 0) data)]))
+
+(defn createImage [image]
+  (let [imgData (take (* image.width image.height) (cycle (range 255)))
+        idv (map vector (iterate inc 0) (genvals image.width image.height))]
+    (doseq [[i v] idv]
+      (aset image.data i v))))
+
+(defn drawSquare [ctx x y width height]
+  (let [imgData (.createImageData ctx width height)]
+    (jsprint imgData)
+    (jsprint (aget imgData "data"))
+    (createImage imgData)
+    (jsprint (aget imgData "data"))
+    (.putImageData ctx imgData x y)))
+
+(let [viewport (.getElementById js/document "viewport")
+      ctx (.getContext viewport "2d")
+      width viewport.width
+      height viewport.height]
+  (drawSquare ctx 100 100 100 100))
+
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
