f949b8f1cbd2f9974bb8efe1237e863be4972364
Henco Appel <hencoappel@gmail.com>
2015-08-07 01:24:11 +0100

    A HSV to RGB converter for nice rainbow effect.

--- src/mandelbrot/core.cljs
+++ src/mandelbrot/core.cljs
@@ -7,14 +7,34 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:rStart -2.5 :rEnd 1.0 :iStart -1.0 :iEnd 1.0}))
 
 (defn jsprint [args]
   (.log js/console args))
 
-(defn genvals [width height]
+(defn convertIterToColour [iter maxIter colourScheme]
+  (colourScheme (/ iter maxIter)))
+
+(defn rainbowColourScheme [iterPercent]
+  (let [h (* iterPercent 6)
+        i (Math/floor h)
+        a 0
+        b (- 1 (- h i))
+        c (- h i)]
+    (if (< (- 1 iterPercent) 0.0001)
+      [0 0 0] ; black
+      (map #(* 255 %)
+           (case i
+             0 [1 c a]
+             1 [b 1 a]
+             2 [a 1 c]
+             3 [a b 1]
+             4 [c a 1]
+             [1 a b])))))
+
+(defn calcMandelbrot [width height]
   (mapcat conj
-            (repeat (vector 255 0 0))
+            (repeat (vector 255))
             (take (* width height) (cycle (range 255)))))
 
 
@@ -23,17 +43,14 @@
 
 (defn createImage [image]
   (let [imgData (take (* image.width image.height) (cycle (range 255)))
-        idv (map vector (iterate inc 0) (genvals image.width image.height))]
+        idv (map vector (iterate inc 0) (calcMandelbrot image.width image.height))]
     (doseq [[i v] idv]
       (aset image.data i v))))
 
 (defn drawSquare [ctx x y width height]
-  (let [imgData (.createImageData ctx width height)]
-    (jsprint imgData)
-    (jsprint (aget imgData "data"))
-    (createImage imgData)
-    (jsprint (aget imgData "data"))
-    (.putImageData ctx imgData x y)))
+  (let [image (.createImageData ctx width height)]
+    (createImage image)
+    (.putImageData ctx image x y)))
 
 (let [viewport (.getElementById js/document "viewport")
       ctx (.getContext viewport "2d")
