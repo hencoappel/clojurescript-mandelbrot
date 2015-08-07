45349a5ad788025a8b3fd01a40973d866328a2bb
Henco Appel <hencoappel@gmail.com>
2015-08-07 11:58:45 +0100

    Add a default value of 100 for the max iter for range

--- src/mandelbrot/mandelbrot.cljs
+++ src/mandelbrot/mandelbrot.cljs
@@ -1,10 +1,7 @@
 (ns ^:figwheel-always mandelbrot.mandelbrot
     (:require))
 
-(defn calcIterations
-  ([real imag]
-   (calcIterations real imag 100))
-  ([real imag maxIter]
+(defn calcIterations [real imag maxIter]
    (loop [r (double 0)
           i (double 0)
           iter (int 0)]
@@ -13,13 +10,18 @@
         (+ (- (* r r) (* i i)) real)
         (+(* 2 r i) imag)
         (inc iter))
-      iter))))
+      iter)))
 
 (defn linspace [start end steps]
   (let [step (/ (- end start) (dec steps))]
     (take steps (map #(+ (* % step) start) (range)))))
 
-(defn calcIterForRange [rStart rEnd rSteps iStart iEnd iSteps]
-  (vec (for [i (linspace iStart iEnd iSteps)]
-      (vec (for [r (linspace rStart rEnd rSteps)]
-        [(calcIterations r i)])))))
+(defn calcIterForRange
+  ([rStart rEnd rSteps iStart iEnd iSteps]
+   (calcIterForRange rStart rEnd rSteps iStart iEnd iSteps 100))
+  ([rStart rEnd rSteps iStart iEnd iSteps maxIter]
+    (vec
+      (for [i (linspace iStart iEnd iSteps)]
+        (vec
+          (for [r (linspace rStart rEnd rSteps)]
+            (calcIterations r i maxIter)))))))
